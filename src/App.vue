41a2e08b8323e18176694bd5b5f3f27e06a4a7bf
Rinat Sarmuldin <ura07srr@gmail.com>
2025-08-24 18:51:37 +0500

    Форма для добавления постов

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="w-1/2 mx-auto p-4">
-      <div>Name: {{ person.name }}</div>
-      <div>Age: {{ person.age }}</div>
-    <div>
-      <span @click="sayOne(222222)">Click</span>
+    <div class="p-4 bg-white border border-gray-200">
+      <div class=mb-4>
+        <input type="text" placeholder="title" class="border-gray-200 p-4 w-full">
+      </div>
+      <div class="mb-4">
+         <textarea placeholder="content" class="border-gray-200 p-4 w-full"></textarea>
+      </div>
+      <div>
+        <a href="#" class="inline-block px-3 py-2 bg-sky-600 border border-sky-700 text-white">ADD POST</a>
+      </div>
     </div>
   </div>
-
 </template>
 
 <script>
