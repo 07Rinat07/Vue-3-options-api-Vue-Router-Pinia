51f8278c63112c1caa912616eab676f656cc21c5
Rinat Sarmuldin <ura07srr@gmail.com>
2025-08-25 14:51:29 +0500

    V-for для выводы карточки

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div class="w-1/2 mx-auto p-4">
-    <div class="p-4 bg-white border border-gray-200">
+    <div class="p-4 mb-4 bg-white border border-gray-200">
       <div class=mb-4>
-        <input type="text" v-model="post.title" placeholder="title" class="border-gray-200 p-4 w-full">
+        <input type="text" v-model="post.title" placeholder="title" class="border border-gray-200 p-4 w-full">
       </div>
       <div class="mb-4">
-         <textarea placeholder="content" v-model="post.content" class="border-gray-200 p-4 w-full"></textarea>
+         <textarea v-model="post.content" placeholder="content" class="border border-gray-200 p-4 w-full"></textarea>
       </div>
       <div>
-        <a @click.prevent="storePost" class="inline-block px-3 py-2 bg-sky-600 border border-sky-700 text-white">ADD POST</a>
+        <a @click.prevent="storePost" href="#" class="inline-block px-3 py-2 bg-sky-600 border border-sky-700 text-white">ADD POST</a>
       </div>
     </div>
+
+    <div>
+        <div v-for="postItem in posts" class="p-4 mb-4 bg-white border border-gray-200">
+      <h3 class="text-lg text-gray-800 mb-2 border-b border-gray-200">{{ postItem.title }}</h3>
+     <p class="text-gray-600 break-words whitespace-pre-line">
+  {{ postItem.content }}
+</p>
+
+    </div>
+  </div>
+
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Our App',
+  name: 'OurApp',
 
   data() {
     return {
