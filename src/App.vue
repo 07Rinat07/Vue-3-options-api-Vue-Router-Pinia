3b04cb29b85108c473c7952b76e7e0adfe9a3c65
Rinat Sarmuldin <ura07srr@gmail.com>
2025-08-26 18:48:13 +0500

    Editor post

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="modal-shadow">
-    <div class="modal">
+  <div @click="editedPost = {}" v-if="Object.keys(editedPost).length > 0" class="modal-shadow">
+    <div @click.stop class="modal">
       <div class="modal-header mb-4">
         <div>
           <h3 class="text-lg">Edit post title</h3>
@@ -9,10 +9,10 @@
       <div class="modal-body">
         <div>
           <div class=mb-4>
-            <input type="text" placeholder="title" class="border border-gray-200 p-4 w-full">
+            <input type="text" v-model="editedPost.title" placeholder="title" class="border border-gray-200 p-4 w-full">
           </div>
           <div class="mb-4">
-            <textarea placeholder="content" class="border border-gray-200 p-4 w-full"></textarea>
+            <textarea v-model="editedPost.content" placeholder="content" class="border border-gray-200 p-4 w-full"></textarea>
           </div>
           <div>
             <a href="#" class="inline-block px-3 py-2 bg-sky-600 border border-sky-700 text-white">Save post</a>
@@ -53,7 +53,7 @@
             {{ postItem.content }}
           </div>
           <div class="flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-2 text-emerald-600 cursor-pointer">
+          <svg @click="editedPost=postItem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mr-2 text-emerald-600 cursor-pointer">
           <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
           </svg>
 
@@ -73,7 +73,8 @@ export default {
   data() {
     return {
       post:{},
-      posts:[]
+      posts:[],
+      editedPost:{},
     }
   },
   methods: {
